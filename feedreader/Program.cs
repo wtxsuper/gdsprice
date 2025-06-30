87b54300882f3a47a8b77788460b6451c38ca56b
Roman Kornoukhov <kornoukhov.omsk@yandex.ru>
2025-06-30 11:55:14 +0600

    Init

---
+++ feedreader/Program.cs
@@ -0,0 +1,43 @@
+﻿using NLog;
+
+namespace feedreader
+{
+    internal class Program
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            try
+            {
+                Logger.Info("Start feed reader module");
+                const string directoryPATH = @"D:\Производственная практика 2025\inFiles";
+                using (FileSystemWatcher watcher = new FileSystemWatcher(directoryPATH))
+                {
+                    watcher.IncludeSubdirectories = true;
+                    watcher.Filter = "*.json";
+                    watcher.Created += OnFileCreated;
+                    watcher.EnableRaisingEvents = true;
+                    Logger.Debug($"Watch files in \"{directoryPATH}\"");
+                    Console.WriteLine("Press enter to exit.");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+            }
+        }
+
+        private static void OnFileCreated(object sender, FileSystemEventArgs e)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+            }
+        }
+    }
+}
