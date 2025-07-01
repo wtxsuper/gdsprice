18d5af8d7c6cfca49643d485b2446c8e70ae6fd4
Roman Kornoukhov <kornoukhov.omsk@yandex.ru>
2025-07-01 12:58:27 +0600

    Add RabbitMQ sending and use top-level operators

--- feedreader/Program.cs
+++ feedreader/Program.cs
@@ -1,117 +1,155 @@
-﻿using Newtonsoft.Json;
+﻿using feedreader;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Schema;
 using NLog;
+using RabbitMQ.Client;
 using System.Collections.Concurrent;
+using System.Text;
 
-namespace feedreader
+Logger logger = LogManager.GetCurrentClassLogger();
+ConcurrentQueue<string> fileQueue = new();
+bool processingFiles = false; // is queue processing?
+
+try
 {
-    internal class Program
+    using FileSystemWatcher watcher = new(Settings.DIRECTORY_PATH);
+    watcher.Filter = "*.json";
+    watcher.Created += OnFileCreated;
+
+    watcher.EnableRaisingEvents = true;
+    logger.Debug($"Watch files in \"{Settings.DIRECTORY_PATH}\"");
+
+    Console.BackgroundColor = ConsoleColor.Green;
+    Console.WriteLine("> Press any key to exit.");
+    Console.ResetColor();
+    _ = Console.ReadKey();
+}
+catch (Exception ex)
+{
+    logger.Error(ex);
+}
+
+void OnFileCreated(object sender, FileSystemEventArgs e)
+{
+    try
     {
-        private const string DIRECTORY_PATH = @"D:\Производственная практика 2025\inFiles";
-        private const string SCHEMA_PATH = @"schema.json";
-        private const int MAX_ATTEMPTS = 5;
+        // Add file to queue
+        fileQueue.Enqueue(e.FullPath);
+        logger.Debug($"File queued for processing: \"{e.Name}\".");
 
-        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
-        private static readonly ConcurrentQueue<string> fileQueue = new ConcurrentQueue<string>();
-        private static bool processingFiles = false; // is queue processing?
+        // Run processing if not running
+        if (!processingFiles)
+        {
+            processingFiles = true;
+            _ = Task.Run(ProcessFilesQueue);
+        }
+    }
+    catch (Exception ex)
+    {
+        logger.Error(ex);
+    }
+}
 
-        static void Main(string[] args)
+async void ProcessFilesQueue()
+{
+    while (fileQueue.TryDequeue(out string? filePath))
+    {
+        int attempt = 1;
+        bool isProcessed = false; // is file successfully opened and processed?
+
+        string filename = Path.GetFileName(filePath);
+
+        while (!isProcessed && attempt <= Settings.MAX_ATTEMPTS)
         {
             try
             {
-                using (FileSystemWatcher watcher = new FileSystemWatcher(DIRECTORY_PATH))
+                // max time to read file
+                CancellationTokenSource cts = new(TimeSpan.FromSeconds(Settings.READ_FILE_TIMEOUT_SECONDS));
+
+                string schemaJson = await File.ReadAllTextAsync(Settings.SCHEMA_PATH, cts.Token);
+                string goodsJson = await File.ReadAllTextAsync(filePath, cts.Token);
+
+
+                JSchema schema = JSchema.Parse(schemaJson);
+                JArray goods = JArray.Parse(goodsJson);
+
+                bool isValid = goods.IsValid(schema, out IList<string> errors);
+
+                if (isValid)
                 {
-                    watcher.IncludeSubdirectories = true;
-                    watcher.Filter = "*.json";
-                    watcher.Created += OnFileCreated;
-
-                    watcher.EnableRaisingEvents = true;
-                    Logger.Debug($"Watch files in \"{DIRECTORY_PATH}\"");
-
-                    Console.WriteLine("Press enter to exit.");
-                    Console.ReadLine();
+                    isProcessed = true;
+                    logger.Debug($"Is valid: \"{filename}\".");
+                    SendData(goodsJson, filename);
+                }
+                else
+                {
+                    isProcessed = true;
+                    logger.Warn($"Not valid: \"{filename}\".\nSchema validation errors: \n" + string.Join('\n', errors));
                 }
             }
             catch (Exception ex)
             {
-                Logger.Error(ex, "Initilization");
-            }
-        }
-        private static void OnFileCreated(object sender, FileSystemEventArgs e)
-        {
-            try
-            {
-                // Add file to queue
-                fileQueue.Enqueue(e.FullPath);
-                Logger.Info($"File queued for processing: {e.FullPath}");
-
-                // Run processing if not running
-                if (!processingFiles)
+                if (ex is IOException && attempt <= Settings.MAX_ATTEMPTS && !isProcessed)
                 {
-                    processingFiles = true;
-                    Task.Run(() => ProcessFilesQueue());
+                    logger.Warn($"Can't open file \"{filename}\". Try again... ({attempt})");
+                    attempt++;
+
+                    // Wait or file process can be blocked
+                    await Task.Delay(500);
                 }
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex);
-            }
-        }
-
-        private static async void ProcessFilesQueue()
-        {
-            while (fileQueue.TryDequeue(out string filePath))
-            {
-                int attempt = 1;
-                bool success = false; // is file successfully opened and process?
-
-                while (!success && attempt <= MAX_ATTEMPTS)
+                else if (ex is TaskCanceledException)
                 {
-                    try
-                    {
-                        string schemaJson = await File.ReadAllTextAsync(SCHEMA_PATH);
-                        string goodsJson = await File.ReadAllTextAsync(filePath);
-
-
-                        JSchema schema = JSchema.Parse(schemaJson);
-                        JArray goods = JArray.Parse(goodsJson);
-
-                        IList<string> errors;
-                        bool isValid = goods.IsValid(schema, out errors);
-
-                        if (isValid)
-                        {
-                            success = true;
-                            Logger.Info($"{filePath} is valid.");
-                            throw new NotImplementedException();
-                        }
-                        else
-                        {
-                            Logger.Error("Schema validation errors: \n" + string.Join('\n', errors));
-                            throw new Exception("Validation error with file " + filePath);
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        if (ex is IOException && attempt <= MAX_ATTEMPTS && !success)
-                        {
-                            Logger.Warn($"Can't open file. Try again... ({attempt})");
-                            attempt++;
-
-                            // Wait or file process can be blocked
-                            await Task.Delay(500);
-                        }
-                        else
-                        {
-                            Logger.Error(ex);
-                            break;
-                        }
-
-                    }
+                    logger.Error(ex, "File read timeout");
+                    break;
                 }
+                else
+                {
+                    logger.Error(ex);
+                    break; // to next file
+                }
+
             }
-            processingFiles = false;
         }
+    }
+    // when queue is empty make flag false
+    processingFiles = false;
+}
+
+async void SendData(string json, string messageName = "")
+{
+    try
+    {
+        string queueName = "toCalc";
+
+        // Connection time limit
+        CancellationTokenSource cts = new(TimeSpan.FromSeconds(Settings.CONNECTION_TIMEOUT_SECONDS));
+        
+        ConnectionFactory factory = new() { HostName = Settings.RABBIT_HOSTNAME };
+        using IConnection connection = await factory.CreateConnectionAsync(cts.Token);
+        using IChannel channel = await connection.CreateChannelAsync();
+
+        _ = await channel.QueueDeclareAsync(queue: queueName, durable: false, exclusive: false, autoDelete: false,
+            arguments: null);
+
+        byte[] body = Encoding.UTF8.GetBytes(json);
+
+        await channel.BasicPublishAsync(exchange: string.Empty, routingKey: queueName, body: body);
+
+        if (string.IsNullOrEmpty(messageName))
+        {
+            logger.Debug("Message sent to broker");
+        }
+        else
+        {
+            logger.Debug($"\"{messageName}\" sent to broker");
+        }
+    }
+    catch (TaskCanceledException ex)
+    {
+        logger.Error(ex, "RabbitMQ connection timeout");
+    }
+    catch (Exception ex)
+    {
+        logger.Error(ex);
     }
 }
---
+++ feedreader/Settings.cs
@@ -0,0 +1,12 @@
+﻿namespace feedreader
+{
+    public static class Settings
+    {
+        public const string DIRECTORY_PATH = @"D:\Производственная практика 2025\inFiles";
+        public const string SCHEMA_PATH = @"schema.json";
+        public const int MAX_ATTEMPTS = 5;
+        public const string RABBIT_HOSTNAME = "localhost";
+        public const int CONNECTION_TIMEOUT_SECONDS = 3;
+        public const int READ_FILE_TIMEOUT_SECONDS = 120;
+    }
+}
