328cf1efe40d3d8df73253a01788ab83ec2f9199
Roman Kornoukhov <kornoukhov.omsk@yandex.ru>
2025-07-01 17:12:22 +0600

    Add module 2: message receiver and quantity calculator

---
+++ QuantityCalculator/Program.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NLog;
+using QuantityCalculator;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Logger logger = LogManager.GetCurrentClassLogger();
+
+try
+{
+    CancellationTokenSource cts = new(TimeSpan.FromSeconds(Settings.CONNECTION_TIMEOUT_SECONDS));
+    string queueName = "toCalc";
+
+    var factory = new ConnectionFactory { HostName = Settings.RABBIT_HOSTNAME };
+    using var connection = await factory.CreateConnectionAsync();
+    using var channel = await connection.CreateChannelAsync();
+
+    await channel.QueueDeclareAsync(queue: queueName, durable: false, exclusive: false, autoDelete: false,
+        arguments: null);
+
+    logger.Debug("Waiting for messages.");
+
+    var consumer = new AsyncEventingBasicConsumer(channel);
+    consumer.ReceivedAsync += OnReceivedAsync;
+
+    await channel.BasicConsumeAsync(queueName, autoAck: true, consumer: consumer);
+
+    Console.BackgroundColor = ConsoleColor.Green;
+    Console.WriteLine("> Press any key to exit.");
+    Console.ResetColor();
+    Console.ReadKey();
+}
+catch (Exception ex)
+{
+    logger.Error(ex);
+}
+
+async Task OnReceivedAsync(object sender, BasicDeliverEventArgs e)
+{
+    var body = e.Body.ToArray();
+    // logger.Debug("Received message. Hash: " + body.GetHashCode());
+    var message = Encoding.UTF8.GetString(body);
+    logger.Warn("Received message: " + message);
+    JArray json = JArray.Parse(message);
+    await ProcessJsonAsync(json);
+}
+
+async Task ProcessJsonAsync (JArray json)
+{
+    foreach (JObject j in json)
+    {
+        Product? product = j.ToObject<Product>();
+        if (product == null)
+        {
+            throw new ArgumentNullException();
+        }
+    }
+}
+
---
+++ QuantityCalculator/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace QuantityCalculator;
+
+public static class Settings
+{
+    public const string RABBIT_HOSTNAME = "localhost";
+    public const int CONNECTION_TIMEOUT_SECONDS = 3;
+}
