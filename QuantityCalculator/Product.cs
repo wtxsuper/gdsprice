4999a030065b7b1ad03fd2feb5139edc934e2482
Roman Kornoukhov <kornoukhov.omsk@yandex.ru>
2025-07-02 11:19:44 +0600

    Classes for JSON deserialization

---
+++ QuantityCalculator/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantityCalculator
+{
+    internal class Product
+    {
+        public required string Id { get; set; }
+        public required string Type { get; set; }
+        public List<Supplier>? Suppliers { get; set; }
+        public List<Product>? SubProducts { get; set; }
+        public List<Warehouse>? Warehouses { get; set; }
+    }
+}
