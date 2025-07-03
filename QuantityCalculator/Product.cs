48adf9ac3e865a27014bc5f78a9afd87b4e6de74
Roman Kornoukhov <kornoukhov.omsk@yandex.ru>
2025-07-03 15:38:49 +0600

    Добавлен семафор для ограничения потоков. Логика подсчёта вынесена в класс. Проверка аргументов на null.

--- QuantityCalculator/Product.cs
+++ QuantityCalculator/Product.cs
@@ -2,8 +2,8 @@
 {
     internal class Product
     {
-        public string? Id { get; set; }
-        public string? Type { get; set; }
+        public string Id { get; set; } = "";
+        public string Type { get; set; } = "";
         public List<Supplier>? Suppliers { get; set; }
         public List<Product>? SubProducts { get; set; }
         public List<Warehouse>? Warehouses { get; set; }
