d1becd27fbd05df8c8864b13738a3274bf22f7c0
Roman Kornoukhov <kornoukhov.omsk@yandex.ru>
2025-07-04 16:02:36 +0600

    Калькуляция цен

--- ClassLibrary/PricedProduct.cs
+++ ClassLibrary/PricedProduct.cs
@@ -1,18 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ClassLibrary
+﻿namespace ClassLibrary
 {
     public class PricedProduct : CountedProduct
     {
-        public bool Test { get; set; } = false;
+        public double MinPrice { get; set; }
 
         public PricedProduct(CountedProduct counted) : base(counted)
         {
-            this.Test = true;
+            double whPrice = 0;
+            double supPrice = 0;
+
+            // Calculte warehouse price
+            if (Type == "product" && Warehouses != null && Warehouses.Count > 0)
+            {
+                whPrice = Warehouses.Average(w => w.Price);
+            }
+            else if (Type == "set" && SubProducts != null && SubProducts.Count > 0)
+            {
+                whPrice = SubProducts.Sum(sp => sp.Warehouses?.Average(w => w.Price) ?? 0);
+            }   
+            else if (Type == "variant" && SubProducts != null && SubProducts.Count > 0)
+            {
+                whPrice = SubProducts.Min(sp => sp.Warehouses?.Average(w => w.Price) ?? 0);
+            }
+
+            // Calculate supplier price
+            if (Type == "product" && Suppliers != null && Suppliers.Count > 0)
+            {
+                supPrice = Suppliers.Min(s => s.Price);            
+            }
+
+            // Determine minimum price
+            if (whPrice > 0 && supPrice > 0)
+            {
+                MinPrice = Math.Min(whPrice, supPrice);
+            }
+            else if (whPrice > 0)
+            {
+                MinPrice = whPrice;
+            }
+            else if (supPrice > 0)
+            {
+                MinPrice = supPrice;
+            }
+            else
+            {
+                MinPrice = 0; // No price available
+            }
         }
     }
 }
